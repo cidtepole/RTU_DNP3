ab3bb6fdbb5b0819a1a67207d9ffdeddf5271911
DELLCID <cidtepole@gmail.com>
2025-07-23 20:16:49 -0600

    version 1.0

---
+++ src/main/java/io/stepfunc/dnp3/examples/ColoresConsola.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.stepfunc.dnp3.examples;
+
+/**
+ *
+ * @author DELLCID
+ */
+public class ColoresConsola {
+    // Códigos ANSI para colores
+    public static final String RESET = "\u001B[0m";
+    public static final String ROJO = "\u001B[31m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String AMARILLO = "\u001B[33m";
+    public static final String AZUL = "\u001B[34m";  
+    
+}
